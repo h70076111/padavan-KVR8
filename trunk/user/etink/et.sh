f209711c17e850c97aed7cf852fad26d79371051
h70076111 <h70076222@2980.com>
2025-12-30 15:28:32 +0800

    Update et.sh

--- trunk/user/etink/et.sh
+++ trunk/user/etink/et.sh
@@ -109,6 +109,7 @@ CMD="$EASYTIER_BIN --network-name $etink_keyg --network-secret $etink_pass -i $e
  [ "$(nvram get et_quic_enable)" = "1" ] && CMD="${CMD} --enable-quic-proxy"
  [ "$(nvram get et_udp_enable)" = "1" ] && CMD="${CMD} --disable-udp-hole-punching"
  [ "$(nvram get et_system_enable)" = "1" ] && CMD="${CMD} --proxy-forward-by-system"
+ [ "$(nvram get et_p2p_enable)" = "1" ] && CMD="${CMD} --disable-p2p"
  [ "$(nvram get et_encryption_enable)" = "1" ] && CMD="${CMD} --disable-encryption"
  [ "$(nvram get et_thread_enable)" = "1" ] && CMD="${CMD} --multi-thread" 
  [ "$(nvram get et_dns_enable)" = "1" ] && CMD="${CMD} --accept-dns"
